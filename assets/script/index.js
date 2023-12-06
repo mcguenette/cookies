4bbf029af30d8047d8e5db8149d6adba46d2257a
MC Guenette <mc.c.guenette@gmail.com>
2023-12-06 09:33:24 -0600

    cleaned up templates

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,29 @@
 'use strict';
 
-import { Cookie } from './Cookie.js';
+import { select, selectAll, onEvent} from './utils.js';
 
+const frontModal = select('#modal-front');
+const backModal = select('#modal-back');
+const settingsButton = select('#settings-btn');
+const acceptButton = select('#accept-btn');
 
+// Modal Functions
+  function showFrontModal() {
+    frontModal.classList.remove('hidden');
+    backModal.classList.add('hidden');
+    overlay.classList.remove('hidden');
+    modal.style.display = 'flex';
+  }
+  
+  function showBackModal() {
+    frontModal.classList.add('hidden');
+    backModal.classList.remove('hidden');
+    modal.style.display = 'flex';
+    startModalCountdown();
+  }
+
+  onEvent('click', settingsButton, showBackModal);
+  setTimeout(showFrontModal, 100);
 
 function getCookie() {
     
